1234bc61d08bd1f32c8b8b9e57155c15e9370ea3
krmhebba <hibakaram2k06@gmail.com>
2026-05-10 14:48:05 +0100

    FM

--- flotte/forms.py
+++ flotte/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Vehicule
+from datetime import date  
 
 class VehiculeForm(forms.ModelForm):
     class Meta:
@@ -9,6 +10,12 @@ class VehiculeForm(forms.ModelForm):
             'immatriculation': forms.TextInput(attrs={'class': 'form-control'}),
             'modele': forms.Select(attrs={'class': 'form-select'}),
             'kilometrage': forms.NumberInput(attrs={'class': 'form-control'}),
-            'etat': forms.Select(attrs={'class': 'form-select'}),
-            'date_fabrication': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'etat': forms.Select(attrs={'class': 'form-select'}),            
+            'date_fabrication': forms.DateInput(
+                format='%Y-%m-%d',
+                attrs={
+                'class': 'form-control', 
+                'type': 'date',
+                'max': date.today().strftime('%Y-%m-%d')
+            }),
         }
